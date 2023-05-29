3385ef79688db4007244d7065377ecf6a0388f18
JPattOfficial <HeSpeaks7@gmail.com>
2023-05-29 05:31:35 -0700

    update

---
+++ Variables and Data Types/Variables and Data Types/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_and_Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a sample program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number, plus 5 is: " +total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInTheBank = 100.5m;
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Jesse";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+
+
+            //jjj
+            Console.WriteLine(questionMark);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
