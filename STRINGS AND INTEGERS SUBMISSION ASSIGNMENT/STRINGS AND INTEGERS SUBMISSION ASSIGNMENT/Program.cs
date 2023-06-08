d441cf0ceb1bc6b4d3a5c0b087434d0c2be2de5e
JPattOfficial <127923501+JPattOfficial@users.noreply.github.com>
2023-06-08 05:26:58 -0700

    Add files via upload

---
+++ STRINGS AND INTEGERS SUBMISSION ASSIGNMENT/STRINGS AND INTEGERS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> intsList = new List<int> { 2, 4, 6, 8 };
+
+        Console.WriteLine("Enter a number to divide the numbers 2, 4, 6, and 8 by: ");
+        string input = Console.ReadLine();
+        int divider = Int32.Parse(input);
+
+        foreach (int num in intsList)
+        {
+            Console.WriteLine(num + " divided by " + divider + " is " + num / divider);
+        }
+        Console.ReadLine();
+
+    }
+}
