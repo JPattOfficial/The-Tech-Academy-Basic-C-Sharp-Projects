1d676afde2a435cba90f6efbec6f2552b6520dc3
JPattOfficial <127923501+JPattOfficial@users.noreply.github.com>
2023-06-09 20:09:39 -0700

    Add files via upload

---
+++ CALLING METHODS SUBMISSION ASSIGNMENT/CALLING METHODS SUBMISSION ASSIGNMENT/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALLING_METHODS_SUBMISSION_ASSIGNMENT
+{
+    public class MathOperations
+    {
+        public int AddTen(int number)
+        {
+            return number + 10;
+        }
+
+        public int MultiplyByFive(int number)
+        {
+            return number * 5;
+        }
+
+        public int SubtractThree(int number)
+        {
+            return number - 3;
+        }
+    }
+
+}
---
+++ CALLING METHODS SUBMISSION ASSIGNMENT/CALLING METHODS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CALLING_METHODS_SUBMISSION_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int result1 = math.AddTen(userInput);
+            Console.WriteLine("Your number + 10 is: " + result1);
+
+            int result2 = math.MultiplyByFive(userInput);
+            Console.WriteLine("Your number * 5 is: " + result2);
+
+            int result3 = math.SubtractThree(userInput);
+            Console.WriteLine("Your number - 3 is: " + result3);
+
+            Console.ReadLine();
+        }
+    }
+}
