8b987f59722974718a966f981c54380d18acd996
JPattOfficial <127923501+JPattOfficial@users.noreply.github.com>
2023-06-09 21:07:08 -0700

    Add files via upload

---
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/MathOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_SUBMISSION_ASSIGNMENT
+{
+    public class MathOperations
+    {
+        public int DoOperation(int number1, int number2 = 10) // Default value for the optional parameter
+        {
+            return number1 + number2; // Adding the numbers
+        }
+    }
+
+}
---
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,32 @@
+﻿using METHOD_SUBMISSION_ASSIGNMENT;
+using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            Console.WriteLine("Enter the first number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number (or just hit enter to skip): ");
+            string number2Input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(number2Input)) // If the user skips the second number
+            {
+                int result = math.DoOperation(number1);
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                int number2 = Convert.ToInt32(number2Input);
+                int result = math.DoOperation(number1, number2);
+                Console.WriteLine("Result: " + result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
