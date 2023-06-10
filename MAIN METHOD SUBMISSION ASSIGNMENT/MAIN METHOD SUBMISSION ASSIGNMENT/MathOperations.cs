d1c102ea7a2d092d6d7388fc6498570949be8baa
JPattOfficial <127923501+JPattOfficial@users.noreply.github.com>
2023-06-09 20:57:11 -0700

    Add files via upload

---
+++ MAIN METHOD SUBMISSION ASSIGNMENT/MAIN METHOD SUBMISSION ASSIGNMENT/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MathOperations
+{
+    public int DoOperation(int number)
+    {
+        return number + 10; // Adding 10 to the number
+    }
+
+    public int DoOperation(decimal number)
+    {
+        return (int)(number / 2); // Dividing the number by 2 and casting to integer
+    }
+
+    public int DoOperation(string number)
+    {
+        int convertedNumber;
+        if (int.TryParse(number, out convertedNumber)) // Try to convert string to integer
+        {
+            return convertedNumber * 3; // Multiplying by 3 if conversion is successful
+        }
+        else
+        {
+            return 0; // Return 0 if conversion fails
+        }
+    }
+}
---
+++ MAIN METHOD SUBMISSION ASSIGNMENT/MAIN METHOD SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            // Calling the method with an integer parameter
+            int result1 = math.DoOperation(5);
+            Console.WriteLine("Result of operation on integer: " + result1);
+
+            // Calling the method with a decimal parameter
+            int result2 = math.DoOperation(7.6m); // 'm' denotes a decimal literal
+            Console.WriteLine("Result of operation on decimal: " + result2);
+
+            // Calling the method with a string parameter
+            int result3 = math.DoOperation("9");
+            Console.WriteLine("Result of operation on string: " + result3);
+
+            Console.ReadLine();
+        }
+    }
+}
