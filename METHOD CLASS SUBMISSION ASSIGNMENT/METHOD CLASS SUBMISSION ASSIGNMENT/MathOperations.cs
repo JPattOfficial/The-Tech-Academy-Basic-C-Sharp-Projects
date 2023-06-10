4d1cd0a46fa959c79ab648428109aa62b3909049
JPattOfficial <127923501+JPattOfficial@users.noreply.github.com>
2023-06-09 21:15:40 -0700

    Add files via upload

---
+++ METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/MathOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace METHOD_CLASS_SUBMISSION_ASSIGNMENT
+{
+    public class MathOperations
+    {
+        public void DoOperation(int number1, int number2)
+        {
+            int result = number1 * 2; // Doing a simple multiplication on the first number
+            Console.WriteLine("Result of math operation: " + result);
+            Console.WriteLine("Second number: " + number2);
+        }
+    }
+
+}
---
+++ METHOD CLASS SUBMISSION ASSIGNMENT/METHOD CLASS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,22 @@
+﻿using METHOD_CLASS_SUBMISSION_ASSIGNMENT;
+using System;
+
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations math = new MathOperations();
+
+            // Call the method in the class, passing in two numbers.
+            math.DoOperation(5, 10);
+
+            // Call the method in the class, specifying the parameters by name.
+            math.DoOperation(number1: 3, number2: 6);
+
+            Console.ReadLine();
+        }
+    }
+}
