d439bf998b5c335a00eda993365c0f52b33b20b4
JPattOfficial <127923501+JPattOfficial@users.noreply.github.com>
2023-06-12 20:28:19 -0700

    Add files via upload

---
+++ PARSING ENUMS SUBMISSION ASSIGNMENT/PARSING ENUMS SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static PARSING_ENUMS_SUBMISSION_ASSIGNMENT.Weekdays;
+
+namespace PARSING_ENUMS_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Console.WriteLine("Please enter the current day of the week: ");
+        string userDay = Console.ReadLine();
+
+        try
+        {
+            // Attempt to convert the user input into a day of the week
+            eWeekdays day = (eWeekdays)Enum.Parse(typeof(eWeekdays), userDay, true);
+            Console.WriteLine("The day of the week you entered is: " + day);
+        }
+        catch (Exception)
+        {
+            // If an error occurs (for instance, if the user didn't input a valid day of the week), print an error message
+            Console.WriteLine("Please enter an actual day of the week.");
+        }
+        Console.ReadLine
+    }
+}
---
+++ PARSING ENUMS SUBMISSION ASSIGNMENT/PARSING ENUMS SUBMISSION ASSIGNMENT/Weekdays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARSING_ENUMS_SUBMISSION_ASSIGNMENT
+{
+    public class Weekdays
+    {
+        //Enum created in seperate class to be called upon easier and for ease of reading code.
+        public enum eWeekdays 
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+        }
+    }
+}
