55c58139660fa95b77f8f15b2c9eef071fb4735d
JPattOfficial <127923501+JPattOfficial@users.noreply.github.com>
2023-06-15 16:47:15 -0700

    Add files via upload

---
+++ LAMBDA SUBMISSION ASSIGNMENT/LAMBDA SUBMISSION ASSIGNMENT/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAMBDA_SUBMISSION_ASSIGNMENT
+{
+    public class Employee
+    {
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public int Id { get; internal set; }
+    }
+}
---
+++ LAMBDA SUBMISSION ASSIGNMENT/LAMBDA SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAMBDA_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { Id = 1, FirstName = "Joe", LastName = "Smith" },
+                new Employee { Id = 2, FirstName = "John", LastName = "Doe" },
+                new Employee { Id = 3, FirstName = "Jane", LastName = "Doe" },
+                new Employee { Id = 4, FirstName = "Joe", LastName = "Johnson" },
+                new Employee { Id = 5, FirstName = "Jack", LastName = "Black" },
+                new Employee { Id = 6, FirstName = "Jill", LastName = "White" },
+                new Employee { Id = 7, FirstName = "Bob", LastName = "Marley" },
+                new Employee { Id = 8, FirstName = "Tom", LastName = "Hanks" },
+                new Employee { Id = 9, FirstName = "Sue", LastName = "Miller" },
+                new Employee { Id = 10, FirstName = "Sara", LastName = "Jones" }
+            };
+
+            // Using foreach loop
+            List<Employee> joes = new List<Employee>();
+            foreach (var employee in employees)
+            {
+                if (employee.FirstName == "Joe")
+                {
+                    joes.Add(employee);
+                }
+            }
+
+            Console.WriteLine("There are " + joes.Count + " employed here.");
+
+            // Using lambda
+            List<Employee> joesLambda = employees.Where(x => x.FirstName == "Joe").ToList();
+            Console.WriteLine("There are " + joesLambda + " Joes employeed here (Lambda)");
+
+            // Employees with Id > 5
+            List<Employee> idGreaterThanFive = employees.Where(x => x.FirstName == "Joe").ToList();
+            Console.WriteLine("There are " + idGreaterThanFive.Count + " employees named Joe here.");
+
+            foreach (Employee joe in idGreaterThanFive)
+            {
+                Console.WriteLine(joe.FirstName + " " + joe.LastName);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
