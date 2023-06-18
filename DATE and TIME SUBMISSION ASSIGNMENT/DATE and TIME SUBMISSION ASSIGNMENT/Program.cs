8d49f734fda39f39f8a3fef59a710f8544c857b2
JPattOfficial <127923501+JPattOfficial@users.noreply.github.com>
2023-06-17 20:45:49 -0700

    Add files via upload

---
+++ DATE and TIME SUBMISSION ASSIGNMENT/DATE and TIME SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get current date and time
+            DateTime now = DateTime.Now;
+            // Print current date and time to console
+            Console.WriteLine("Current date and time: " + now);
+
+            // Ask the user for a number
+            Console.Write("Enter a number of hours: ");
+            string input = Console.ReadLine();
+
+            // Convert input string to integer
+            int hours;
+            if (Int32.TryParse(input, out hours))
+            {
+                // Calculate time after x hours
+                DateTime futureTime = now.AddHours(hours);
+                // Print future time to console
+                Console.WriteLine("In " + hours + " hours, it will be: " + futureTime);
+            }
+            else
+            {
+                // Handle invalid input
+                Console.WriteLine("Invalid input! Please enter a valid number.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
