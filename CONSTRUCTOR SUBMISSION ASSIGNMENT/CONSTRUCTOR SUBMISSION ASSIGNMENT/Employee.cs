bc833065a135d34c0d92585af3711dc5626a6f37
JPattOfficial <127923501+JPattOfficial@users.noreply.github.com>
2023-06-19 22:34:08 -0700

    Add files via upload

---
+++ CONSTRUCTOR SUBMISSION ASSIGNMENT/CONSTRUCTOR SUBMISSION ASSIGNMENT/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSTRUCTOR_SUBMISSION_ASSIGNMENT
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        // First constructor, it calls the second constructor
+        public Employee(string name) : this(name, "N/A")
+        {
+        }
+
+        // Second constructor
+        public Employee(string name, string department)
+        {
+            Name = name;
+            Console.WriteLine("Employee's name: " + Name);
+            Console.WriteLine("Employee's department: " + department);
+        }
+    }
+}
---
+++ CONSTRUCTOR SUBMISSION ASSIGNMENT/CONSTRUCTOR SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSTRUCTOR_SUBMISSION_ASSIGNMENT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int constVar = 10;
+            Console.WriteLine("Const Variable: " + constVar);
+            Console.ReadLine();
+
+            var someVar = "Hello World!";
+            Console.WriteLine("Var Variable: " + someVar);
+
+            Employee employee = new Employee("John Doe");
+
+            Console.ReadLine();
+        }
+    }
+}
